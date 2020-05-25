d8f572d4a9aea70eae9985ddc038f1d2a6f672b6
Igor Freitas <Freitas@Igors-MacBook-Pro-2.local>
2020-05-25 16:45:32 -0300

    ORC OFFLINE TESTE

---
+++ tesseractApp/src/pages/home/home.ts
@@ -0,0 +1,45 @@
+import { Component } from "@angular/core";
+import { OcrProvider } from "../../providers/ocr/ocr";
+import { AlertController, LoadingController } from "ionic-angular";
+
+@Component({
+  selector: "page-home",
+  templateUrl: "home.html",
+})
+export class HomePage {
+  constructor(
+    private ocrProvider: OcrProvider,
+    private loadingController: LoadingController,
+    private alertController: AlertController
+  ) {}
+
+  public async recognize(img): Promise<void> {
+    const loading = await this.loadingController.create({
+      content: "Analyzing image",
+    });
+
+    await loading.present();
+
+    try {
+      const value = await this.ocrProvider.text(img.srcElement, (progress) => {
+        const percentage = Math.round(progress * 100);
+
+        loading.setContent(`Analysing image (${percentage}%)`);
+      });
+
+      const alert = await this.alertController.create({
+        title: "OCR complete",
+        inputs: [
+          {
+            type: "textarea",
+            value,
+          },
+        ],
+      });
+
+      await alert.present();
+    } finally {
+      await loading.dismiss();
+    }
+  }
+}
